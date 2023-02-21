38005ebc3a040f987c7ec62f73c646b9d4dfbe79
priyalgupta <66379600+priyalgupta@users.noreply.github.com>
2023-02-21 16:04:06 +0530

    updated

---
+++ frontend/build/precache-manifest.4f35a8638623f6dd5ac1394a35e66fb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b436ceafaa688203556fd862c54ea34f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e01863b889dd58dd636f",
+    "url": "/static/css/main.91efec17.chunk.css"
+  },
+  {
+    "revision": "4f3dd13e3687c67590df",
+    "url": "/static/js/2.df7aa9d6.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/static/js/2.df7aa9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e01863b889dd58dd636f",
+    "url": "/static/js/main.e9573df3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e9573df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
